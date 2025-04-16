c851f2c93712033f30c4ae92cebf2adef89fac76
Ahmad Alhariri <65652515+0Ahmad0@users.noreply.github.com>
2025-04-16 23:42:44 +0300

    Update settings.py

--- LEAFNET/settings.py
+++ LEAFNET/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=_5_+n&*cfv2%v7lchbs6xpowm++x%#$dt023k+nh=7wd=8+ip'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
